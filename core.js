22e10244c8707c936377e54a2d472660ebb63fec
Patricia Nunes <patricia@krint.io>
2019-03-10 18:21:06 +1100

    Initial commit

---
+++ core.js
@@ -0,0 +1,121 @@
+// utilities
+// -------------------------------------------------------
+let i = -1;
+const next = () => {
+  i = i + 1
+  return i;
+};
+
+
+// API
+// -------------------------------------------------------
+
+// Position = { row: number, column: number } ;
+// Card = string;
+// Spot = { card: Card, isOpen: boolean, hasMatched: boolean };
+// Board = Spot[][];
+// State = { board: Board, isGameOver: boolean };
+// Action = { type: string, payload: any };
+const initialState = {
+  board: [
+    [
+      { card:'🤯', isOpen: false, hasMatched: false },
+      { card:'👩‍💻', isOpen: false, hasMatched: false },
+    ],
+    [
+      { card:'👩‍💻', isOpen: false, hasMatched: false },
+      { card:'🤯', isOpen: false, hasMatched: false },
+    ],
+  ],
+  isGameOver: false,
+};
+
+// createNewGame :: Card[] -> Position -> State
+export const createNewGame = (cards, format) => {
+  const deck = createDeck(cards); // [ '👩‍💻', '🤯', '👩‍💻', '🤯' ]
+
+  // shuffle :: deck -> deck
+  // shuffle(deck)
+
+  const board = createBoard(deck, format); // 
+
+  return {
+    board,
+    isGameOver: false,
+  };
+};
+
+// play :: State -> Action -> State
+export const play = (state, action) => {
+  switch (action.type) {
+    case 'PLAY':
+      const { row, column } = action.payload;
+      let boardCopy = state.board.slice()
+        .map((r, rIndex) =>
+          r.map((c, cIndex) =>
+            (row === rIndex && column === cIndex) ? {...c, isOpen: true} : c ));
+
+      const spot = Object.assign({}, boardCopy[row][column]);
+      const matched = isThereAMatch(spot, state.board);
+      const spotsOpen = state.board.flat().filter(s => s.isOpen);
+      const spotsMatched = state.board.flat().filter(s => s.hasMatched);
+
+      if (matched) {
+        boardCopy = boardCopy
+          .map(r =>
+            r.map(c => c.card === spot.card ? {...c, hasMatched: true} : c));
+      } else {
+
+        boardCopy = boardCopy
+          .map(r => 
+            r.map(c => c.isOpen && !c.hasMatched && spotsOpen.length > spotsMatched ? {... c, isOpen : false} : c));
+      }
+
+      // if open (isOpen?), do nothing
+      // open(row, column)
+
+      return {
+        ...state,
+        board: [...boardCopy],
+        isGameOver: boardCopy.flat().every(s => s.hasMatched),
+      };
+    default:
+      return state;
+  }
+};
+
+
+// private functions
+// -------------------------------------------------------
+
+// createBoard :: Deck -> Board
+const createBoard = (deck, format) => {
+  const spots = deck.map(makeSpot);
+  return breakdown(spots, format);
+};
+
+// createDeck :: Card[] -> Card[]
+const createDeck = cards => [...cards, ...cards];
+
+// breakdown :: Spot[] -> Position -> Board
+const breakdown = (spots, format) => {
+  const { row, column } = format;
+  const rows = Array.from(new Array(row));
+  const columns = Array.from(new Array(column));
+  const _spots = rows.map(_ => columns);
+  return _spots.map(row => row.map(_ => spots[next()]));
+};
+
+// isThereAMatch :: Spot -> Board -> boolean
+const isThereAMatch = (spot, board) => 
+  board
+    .flat()
+    .filter(s => spot.card === s.card)
+    .some(s => s.isOpen === true);
+
+// makeSpot :: Card -> Spot
+const makeSpot = card => ({
+  card,
+  isOpen: false,
+  hasMatched: false,
+});
---
+++ tests.js
@@ -0,0 +1,72 @@
+import { createNewGame, play } from './core';
+
+const state0 = createNewGame(['👩‍💻', '🤯'], { row: 2, column: 2 });
+console.log(state0);
+
+const action0 = {
+  type: 'PLAY',
+  payload: {
+    row: 0,
+    column: 1,
+  },
+};
+
+const state1 = play(state0, action0);
+console.log(state1.board.flat());
+
+
+const state2 = play(state1, {
+  type: 'PLAY',
+  payload: {
+    row: 0,
+    column: 0,
+  },
+});
+console.log(state2);
+const state2 = play(state1, {
+  type: 'PLAY',
+  payload: {
+    row: 1,
+    column: 1,
+  },
+});
+console.log(state2);
+
+const state3 = play(state2, {
+  type: 'PLAY',
+  payload: {
+    row: 0,
+    column: 0,
+  },
+});
+console.log(state3.board.flat());
+
+const state4 = play(state3, {
+  type: 'PLAY',
+  payload: {
+    row: 1,
+    column: 0,
+  },
+});
+console.log(state4.board.flat());
+
+const state5 = play(state4, {
+  type: 'PLAY',
+  payload: {
+    row: 0,
+    column: 1,
+  },
+});
+console.log(state5.board.flat());
+
+const state6 = play(state5, {
+  type: 'PLAY',
+  payload: {
+    row: 1,
+    column: 1,
+  },
+});
+console.log(state6);
+
+// createNewGame(['👩‍💻', '🤯', '🧠', '🤰🏻', '😱', '🇧🇷'], { row: 3, column: 4 });
+// createNewGame(['👩‍💻', '🤯', '🧠', '🤰🏻', '😱', '🇧🇷', '🐷', '👵🏻'], { row: 4, column: 4 });
